d9125072a5120fd0d6c9a18df7562904571824d8
Gregory Amerson <gregory.amerson@liferay.com>
2016-06-24 10:37:27 -0500

    [progress] safer synchronization
    Signed-off-by: Gregory Amerson <gregory.amerson@liferay.com>

--- bndtools.core/src/bndtools/central/Central.java
+++ bndtools.core/src/bndtools/central/Central.java
@@ -3,10 +3,10 @@ package bndtools.central;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -58,8 +58,8 @@ public class Central implements IStartupParticipant {
 
     private static Central instance = null;
 
-    static Workspace workspace = null;
-    static final List<Function<Workspace,Void>> workspaceInitCallbackQueue = new LinkedList<Function<Workspace,Void>>();
+    static volatile Workspace workspace = null;
+    static final ConcurrentLinkedQueue<Function<Workspace,Void>> callbacks = new ConcurrentLinkedQueue<>();
 
     static WorkspaceR5Repository r5Repository = null;
 
@@ -194,60 +194,65 @@ public class Central implements IStartupParticipant {
         }
     }
 
-    public synchronized static Workspace getWorkspace() throws Exception {
-        if (instance == null)
-            throw new IllegalStateException("Central has not been initialised");
+    public static Workspace getWorkspace() throws Exception {
+        synchronized (Central.class) {
+            if (instance == null)
+                throw new IllegalStateException("Central has not been initialised");
 
-        if (workspace != null)
-            return workspace;
+            if (workspace != null)
+                return workspace;
 
-        Workspace newWorkspace = null;
+            Workspace newWorkspace = null;
 
-        try {
-            Workspace.setDriver(Constants.BNDDRIVER_ECLIPSE);
-            Workspace.addGestalt(Constants.GESTALT_INTERACTIVE, new Attrs());
+            try {
+                Workspace.setDriver(Constants.BNDDRIVER_ECLIPSE);
+                Workspace.addGestalt(Constants.GESTALT_INTERACTIVE, new Attrs());
 
-            newWorkspace = Workspace.getWorkspace(getWorkspaceDirectory());
+                newWorkspace = Workspace.getWorkspace(getWorkspaceDirectory());
 
-            newWorkspace.addBasicPlugin(new WorkspaceListener(newWorkspace));
-            newWorkspace.addBasicPlugin(instance.repoListenerTracker);
-            newWorkspace.addBasicPlugin(getWorkspaceR5Repository());
-            newWorkspace.addBasicPlugin(new JobProgress());
+                newWorkspace.addBasicPlugin(new WorkspaceListener(newWorkspace));
+                newWorkspace.addBasicPlugin(instance.repoListenerTracker);
+                newWorkspace.addBasicPlugin(getWorkspaceR5Repository());
+                newWorkspace.addBasicPlugin(new JobProgress());
 
-            // Initialize projects in synchronized block
-            newWorkspace.getBuildOrder();
+                // Initialize projects in synchronized block
+                newWorkspace.getBuildOrder();
 
-            // Monitor changes in cnf so we can refresh the workspace
-            addCnfChangeListener(newWorkspace);
+                // Monitor changes in cnf so we can refresh the workspace
+                addCnfChangeListener(newWorkspace);
 
-            workspaceRepositoryChangeDetector = new WorkspaceRepositoryChangeDetector(newWorkspace);
+                workspaceRepositoryChangeDetector = new WorkspaceRepositoryChangeDetector(newWorkspace);
 
-            // Call the queued workspace init callbacks
-            synchronized (workspaceInitCallbackQueue) {
                 // The workspace has been initialized fully, set the field now
                 workspace = newWorkspace;
-
-                while (!workspaceInitCallbackQueue.isEmpty()) {
-                    Function<Workspace,Void> callback = workspaceInitCallbackQueue.remove(0);
-                    callback.apply(workspace);
+            } catch (final Exception e) {
+                if (newWorkspace != null) {
+                    newWorkspace.close();
                 }
+                throw e;
             }
-
-            return workspace;
-        } catch (final Exception e) {
-            if (newWorkspace != null) {
-                newWorkspace.close();
-            }
-            throw e;
         }
+
+        notifyCallbacks();
+
+        return workspace;
     }
 
     public static void onWorkspaceInit(Function<Workspace,Void> callback) {
-        synchronized (workspaceInitCallbackQueue) {
-            if (workspace != null)
+        callbacks.add(callback);
+        notifyCallbacks();
+    }
+
+    private static void notifyCallbacks() {
+        if (workspace == null)
+            return;
+
+        for (Function<Workspace,Void> callback = callbacks.poll(); callback != null; callback = callbacks.poll()) {
+            try {
                 callback.apply(workspace);
-            else
-                workspaceInitCallbackQueue.add(callback);
+            } catch (Throwable t) {
+                //log the exception
+            }
         }
     }
 
